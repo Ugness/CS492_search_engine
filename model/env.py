2a1b19ba105d62fd694b0ce528d0118f93ef0924
Ugness <wogns98@kaist.ac.kr>
2023-06-11 12:44:11 +0000

    iterative training

--- model/env.py
+++ model/env.py
@@ -20,55 +20,57 @@ class Env(nn.Module):
         self.a_idx = 0
         self.orig_length = indices.clone()
         self.gt_y = gt_y
-        return data, indices, current_nodes
+        return data, indices-1, current_nodes # indices point to </s>
     
-    def compute_reward(self, obs, already_done, requires_grad=False):
+    def compute_reward(self, obs, already_done, reward_type='acc'):
         # TODO:
         # check whether done or not.
         # if done, compute reward.
         #   re-organize 
-        grad_control = torch.enable_grad if requires_grad else torch.no_grad
+        done = (self.answer==self.tokenizer.eos_token_id).any(-1)
+        done = done | already_done
+        B = self.answer.shape[0]
+        reward = torch.zeros(B, device=obs.device, requires_grad=False)
+        correct = torch.zeros(B, device=obs.device, requires_grad=False).bool()
+        if (~already_done*done).any():
+            # compute reward
+            # decode answer
+            ids = self.tokenizer.batch_decode(self.answer, skip_special_tokens=True)
+            ids = [id.replace(' ', '') for id in ids]
 
-        with grad_control():
-            done = self.answer[:, -1] == self.tokenizer.eos_token_id
-            B = self.answer.shape[0]
-            reward = torch.zeros(B, device=obs.device)
-            if done.any():
-                # compute reward
-                # decode answer
-                ids = self.tokenizer.batch_decode(self.answer, skip_special_tokens=True)
-                ids = [id.replace(' ', '') for id in ids]
+            # parse if possible
+            data = [[0 for _ in range(10)] if not id in self.database else self.database[id] for id in ids]
+            data = [''.join(map(str, d)) for d in data]
+            data = self.tokenizer(data, return_tensors='pt').input_ids
 
-                # parse if possible
-                data = [[0 for _ in range(10)] if not id in self.database else self.database[id] for id in ids]
-                data = [''.join(map(str, d)) for d in data]
-                data = self.tokenizer(data, return_tensors='pt').input_ids
+            # construct input of reader
+            # format: [full_expression, <pad>, x, <pad>, y0, y1, ..., y9, <eos>]
+            inputs = torch.zeros_like(obs)
+            targets = torch.zeros_like(obs)
+            masks = torch.zeros_like(obs).bool()
+            for b in range(B):
+                inputs[b, :self.orig_length[b]] = obs[b, :self.orig_length[b]]
+                # convert eos to pad
+                assert inputs[b, self.orig_length[b]-1] == self.tokenizer.eos_token_id, f'{inputs[b, self.orig_length[b]-1]}'
+                inputs[b, self.orig_length[b]-1] = self.tokenizer.pad_token_id
+                inputs[b, self.orig_length[b]:self.orig_length[b]+10] = data[b]
+                inputs[b, self.orig_length[b]+10] = self.tokenizer.eos_token_id
+                masks[b, self.orig_length[b]+10] = True
+                targets[b, self.orig_length[b]+10] = self.gt_y[b]
 
-                # construct input of reader
-                # format: [full_expression, <pad>, x, <pad>, y0, y1, ..., y9, <eos>]
-                inputs = torch.zeros_like(obs)
-                targets = torch.zeros_like(obs)
-                masks = torch.zeros_like(obs).bool()
-                for b in range(B):
-                    inputs[b, :self.orig_length[b]] = obs[b, :self.orig_length[b]]
-                    # convert eos to pad
-                    assert inputs[b, self.orig_length[b]-1] == self.tokenizer.eos_token_id
-                    inputs[b, self.orig_length[b]-1] = self.tokenizer.pad_token_id
-                    inputs[b, self.orig_length[b]:self.orig_length[b]+10] = data[b]
-                    inputs[b, self.orig_length[b]+10] = self.tokenizer.eos_token_id
-                    masks[b, self.orig_length[b]+10] = True
-                    targets[b, self.orig_length[b]+10] = self.gt_y[b]
-                # compute reward
-                with torch.no_grad():
-                    reward = self.reader(inputs, masks=masks, targets=targets, reduce=False)[1]
-                reward = -reward
-                # re-organize reward
-                reward = reward * done.float()
+            # compute reward
+            logit, reward = self.reader(inputs, masks=masks, targets=targets, reduce=False)
+            reward = -reward
+            pred = logit[masks].argmax(dim=-1)
+            correct = (pred == self.gt_y)
+            # re-organize reward
+            reward = reward * done.float()
+            if reward_type == 'acc':
+                reward = correct.float() * done.float()
             reward[already_done] = 0.
-            return reward, done
+        return reward, done, correct
     
-    @torch.no_grad()
-    def step(self, obs, indices, current_nodes, action, already_done=False):
+    def step(self, obs, indices, current_nodes, action, already_done=False, reward_type='acc'):
         # TODO:
         #   if action == eos and not already done: done = True, compute reward.
         #       decode obs, and parse database. construct input of reader.
@@ -78,15 +80,15 @@ class Env(nn.Module):
 
         # update node
         current_nodes = [current_nodes[b].children[action[b].item()] for b in range(B)]
-        # update obs
-        obs = torch.scatter(obs, dim=1, index=indices.view(-1, 1), src=action.view(-1, 1))
         # update indices
-        indices[action != self.tokenizer.eos_token_id] = indices[action != self.tokenizer.eos_token_id] + 1
+        indices[~already_done] = indices[~already_done] + 1
+        # append action to obs
+        obs = torch.scatter(obs, dim=1, index=indices.view(-1, 1), src=action.view(-1, 1))
         # update answer
         self.answer[:, self.a_idx] = action
         self.a_idx += 1
 
         # compute_reward
-        reward, done = self.compute_reward(obs, already_done)
+        reward, done, correct = self.compute_reward(obs, already_done, reward_type=reward_type)
 
-        return obs, indices, current_nodes, reward, done, None
+        return obs, indices, current_nodes, reward, done, correct
--- train_rl.py
+++ train_rl.py
@@ -4,16 +4,82 @@ import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
+def train_policy_gradient_iterative(
+        act_model,
+        act_opt,
+        env_opt,
+        train_loader,
+        env,
+        gamma=1.0,
+        temperature=1.0,
+        top_k=None,
+        top_p=None,
+        writer=None,
+        reward_type='acc',):
+    pbar = tqdm(train_loader)
+    for i, (data, indices, gt_y) in enumerate(train_loader):
+        data, indices, gt_y = data.cuda(), indices.cuda(), gt_y.cuda()
+        obs, indices, current_nodes = env.reset(data, indices, gt_y)
+        B = len(obs)
+        done = torch.zeros(len(data), dtype=torch.bool, device=obs.device, requires_grad=False)
+        episode_reward = torch.zeros(len(data), device=data.device, requires_grad=False)
+        log_probs = []
+        rewards = []
+        terminates = []
+        while not done.all():
+            action_logits, action_probs, log_prob, action = act_model.predict(obs, indices, current_nodes, env.trie,
+                                                                              temperature, top_k, top_p)
+            terminates.append(done)
+            obs = obs.detach().clone()
+            indices = indices.detach().clone()
+            with torch.no_grad():
+                obs, indices, current_nodes, reward, done, correct = env.step(obs, indices, current_nodes, action, done, reward_type)
+            episode_reward += reward
+            log_probs.append(log_prob)
+            rewards.append(reward.detach())
+        returns = []
+        G = 0
+        for r in reversed(rewards):
+            G = r + gamma * G
+            returns.insert(0, G)
+        returns = torch.stack(returns, -1) # (B, T)
+        log_probs = torch.stack(log_probs, -1)
+        terminates = torch.stack(terminates, -1).float()
+        advantages = returns - returns.mean(-1, keepdim=True)
+        # sum over timesteps, mean over episodes
+        policy_loss = -(log_probs * advantages.detach() * (1.-terminates)).mean(0).sum(0)
+        act_opt.zero_grad()
+        loss = policy_loss
+        loss.backward()
+        act_opt.step()
+
+        # update env
+        obs, indices, current_nodes, reward, done, correct = env.step(obs, indices, current_nodes, action, torch.zeros_like(done), 'ce')
+        ce_loss = -reward.mean()
+        acc = correct.float().mean()
+        env_opt.zero_grad()
+        ce_loss.backward()
+        env_opt.step()
+
+        if writer is not None:
+            writer.add_scalar('Loss/policy_loss', policy_loss.item(), i)
+            writer.add_scalar('Loss/ce_loss', ce_loss.item(), i)
+            writer.add_scalar('Reward/episode_reward', episode_reward.mean().item(), i)
+            writer.add_scalar('Accuracy/episode_accuracy', acc.item(), i)
+        pbar.set_description(f"Episode {i+1}: reward={episode_reward.mean().item():.4f}, policy_loss={policy_loss.item():.4f}, accuracy={acc.item():.4f}")
+        pbar.update()
+
 def train_policy_gradient(
         act_model,
         act_opt,
         train_loader,
         env,
-        gamma=0.99,
+        gamma=1.0,
         temperature=1.0,
         top_k=None,
         top_p=None,
-        writer=None):
+        writer=None,
+        reward_type='acc',):
 
     pbar = tqdm(train_loader)
     for i, (data, indices, gt_y) in enumerate(train_loader):
@@ -24,14 +90,21 @@ def train_policy_gradient(
         episode_reward = torch.zeros(len(data), device=data.device, requires_grad=False)
         log_probs = []
         rewards = []
+        terminates = []
         while not done.all():
             action_logits, action_probs, log_prob, action = act_model.predict(obs, indices, current_nodes, env.trie,
                                                                               temperature, top_k, top_p)
-            obs, indices, current_nodes, reward, done, _ = env.step(obs, indices, current_nodes, action, done)
-            episode_reward += reward.detach()
+            terminates.append(done) # to compute loss properly.
+            obs = obs.detach().clone()
+            indices = indices.detach().clone()
+            with torch.no_grad():
+                obs, indices, current_nodes, reward, done, correct = env.step(obs, indices, current_nodes, action, done, reward_type)
+            episode_reward += reward
             log_probs.append(log_prob)
             rewards.append(reward.detach())
         returns = []
+        acc = correct.float().mean()
+        terminates = torch.stack(terminates, -1).float()
         G = 0
         for r in reversed(rewards):
             G = r + gamma * G
@@ -39,18 +112,18 @@ def train_policy_gradient(
         returns = torch.stack(returns, -1) # (B, T)
         log_probs = torch.stack(log_probs, -1)
         advantages = returns - returns.mean(-1, keepdim=True)
-        policy_loss = -(log_probs * advantages.detach()).mean()
+        # sum over timesteps, mean over episodes
+        policy_loss = -(log_probs * advantages.detach() * (1.-terminates)).mean(0).sum(0)
         act_opt.zero_grad()
         loss = policy_loss
         loss.backward()
         act_opt.step()
         if writer is not None:
             writer.add_scalar('Loss/policy_loss', policy_loss.item(), i)
-            writer.add_scalar('Reward/episode_reward', episode_reward, i)
-
+            writer.add_scalar('Reward/episode_reward', episode_reward.mean().item(), i)
+            writer.add_scalar('Accuracy/episode_accuracy', acc.item(), i)
+        pbar.set_description(f"Episode {i+1}: reward={episode_reward.mean().item():.4f}, policy_loss={policy_loss.item():.4f}, accuracy={acc.item():.4f}")
         pbar.update()
-        pbar.set_description(f"Episode {i+1}: reward={episode_reward}, policy_loss={policy_loss.item()}")
-        # print(f"Episode {i+1}: reward={episode_reward}, policy_loss={policy_loss.item()}")
 
 def train_q_learning(model, optimizer, env, num_episodes, gamma=0.99):
     for i in range(num_episodes):
@@ -71,3 +144,4 @@ def train_q_learning(model, optimizer, env, num_episodes, gamma=0.99):
             optimizer.step()
             obs = next_obs
         print(f"Episode {i+1}: reward={episode_reward}, epsilon={model.exploration_rate}")
+        
